13e0aa39a57d729f089a1b7d2ee6f4dc2e43c31f
Mark Kramer <markkra@outlook.com>
2016-05-28 16:52:03 -0700

    Expose dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'build'),
-    publicPath: "/",
+    publicPath: "/markkra.github.io/",
     filename: 'bundle.js'
   },
   plugins: [
